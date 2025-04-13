8d30cc4dfe8a7a9077cfac5b9c4e286a80e36eba
andrew <pochta.andrew@gmail.com>
2025-04-13 16:02:26 +0500

    first webinar as intro

---
+++ src/com/colvir/webinar01/Launcher.java
@@ -0,0 +1,44 @@
+package com.colvir.webinar01;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Launcher {
+
+    private static String readFromFile(String pFileName ){
+        String sContent = "";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(pFileName));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            reader.close();
+            sContent = stringBuilder.toString();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return sContent;
+    }
+
+    public static void main(String[] args) {
+        String inputFile = "resources\\input.txt";
+        String content = readFromFile(inputFile);
+        String regex = "[ \\n,.!\\s]";
+        // String result[] = content.split(regex, -1);
+        String[] result = Arrays.stream(content.split(regex))
+                            .filter(str -> !str.isEmpty())
+                            .map(String::toLowerCase)
+                            .toList()
+                            .stream().sorted()
+                            .toArray(String[]::new);
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
+}
